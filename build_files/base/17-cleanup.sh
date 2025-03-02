97678429d9a387924a7564e29fa35d12bf91c948
Adam Fidel <adam@fidel.id>
2025-03-02 14:08:04 -0600

    chore: use systemd preset to enable units

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -4,25 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Setup Systemd
-systemctl enable rpm-ostree-countme.service
-systemctl enable tailscaled.service
-systemctl enable dconf-update.service
-systemctl --global enable ublue-flatpak-manager.service
-systemctl enable rpm-ostreed-automatic.timer
-systemctl enable flatpak-system-update.timer
-systemctl --global enable flatpak-user-update.timer
-systemctl enable ublue-system-setup.service
-systemctl enable ublue-guest-user.service
-systemctl enable brew-setup.service
-systemctl enable brew-upgrade.timer
-systemctl enable brew-update.timer
-systemctl enable aurora-groups.service
-systemctl enable usr-share-sddm-themes.mount
-systemctl --global enable ublue-user-setup.service
-systemctl --global enable podman-auto-update.timer
-systemctl enable check-sb-key.service
-
 # Hide Desktop Files. Hidden removes mime associations
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/fish.desktop
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nHidden=true@g' /usr/share/applications/htop.desktop
